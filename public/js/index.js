4bc4cd9549272d00febdf878da31b21d9dc6f015
guntapilus <guntapilus@noreply.users.github.com>
2016-12-21 23:46:20 +0700

    more mockup room

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,20 @@ var vm = new Vue({
 	el: '#roomList',
 	data:{
 		currentView:screenEnum.ROOMLIST,
-		rooms:[{
-			id: 1,
-			name: 'sample room',
-			playerNo: '3',
-			status:'Running',
-		}]
+		rooms:[
+			{
+				id: 1,
+				name: 'sample room',
+				playerNo: '3',
+				status:'Running',
+			},
+			{
+				id: 2,
+				name: 'second room',
+				playerNo: '2',
+				status:'Waiting for Player',
+			}
+		],
 	},
 	computed:{
 
