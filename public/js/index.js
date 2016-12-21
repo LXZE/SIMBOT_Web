e35c91caeb2b5b051d085743579fed23921ca896
guntapilus <guntapilus@noreply.users.github.com>
2016-12-21 23:44:19 +0700

    revert to the version before separate template to different file

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,16 @@ var screenEnum={
 	ROOMLIST:0,
 	CREATEROOM:1,
 }
-function getRoomList(){
-	//mock up data
-	return sampleRoomsList;
-}
-Vue.component('room-list-pane',{
-	template:document.getElementById('roomListTemplate').innerHTML,
-	data:{
-		rooms: function(){
-			return getRoomList()
-		},
-		aroom:function(){
-			return sampleRoomsList[0]
-		},
-	}
-})
 var vm = new Vue({
-	el: '#app',
+	el: '#roomList',
 	data:{
 		currentView:screenEnum.ROOMLIST,
+		rooms:[{
+			id: 1,
+			name: 'sample room',
+			playerNo: '3',
+			status:'Running',
+		}]
 	},
 	computed:{
 
@@ -29,10 +20,3 @@ var vm = new Vue({
 
 	},
 })
-
-var sampleRoomsList = [{
-	id: 1,
-	name: 'sample room',
-	playerNo: '3',
-	status:'Running',
-}]
