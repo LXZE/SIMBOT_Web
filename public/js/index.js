c611a62f3d29bbbbb8dd63470419eb37ab1cabde
guntapilus <guntapilus@noreply.users.github.com>
2016-12-21 23:28:34 +0700

    fix typo

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ var screenEnum={
 	CREATEROOM:1,
 }
 Vue.component('room-list-pane',{
-	template:Document.getElementById('roomListTemplate').innerHTML(),
+	template:document.getElementById('roomListTemplate').innerHTML(),
 	data:{
 		rooms:[{
 			id: 1,
