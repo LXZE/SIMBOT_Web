17bc7d1e054ef367ac9647fc2e8d91c604f26007
guntapilus <guntapilus@noreply.users.github.com>
2017-01-09 16:40:07 +0700

    change room id to string

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,13 @@ var vm = new Vue({
 		currentView:screenEnum.ROOMLIST,
 		rooms:[
 			{
-				id: 1,
+				id: '1',
 				name: 'sample room',
 				playerNo: '3',
 				status:'Running',
 			},
 			{
-				id: 2,
+				id: '2',
 				name: 'second room',
 				playerNo: '2',
 				status:'Waiting for Player',
